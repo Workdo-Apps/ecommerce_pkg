5f663e4fda7892fc668ed99fb0bcdd4b0c9102c5
dolly-flu <dollyrajodiya@gmail.com>
2024-04-25 17:09:46 +0530

    add store name

--- lib/utils/store_name.dart
+++ lib/utils/store_name.dart
@@ -19,15 +19,19 @@ class AppStoreName {
   static String books = '/books';
   static String motorcycle = '/motorcycle';
   static String kitchen = '/kitchen';
-
-  static String dentist = '/dentist';
-  static String pharmacy = '/pharmacy';
-  static String nutritions = '/nutritions';
-  static String cosmeticshospitals = '/cosmeticshospitals';
-  static String clinicshospitals = '/clinicshospitals';
-  static String spabeauty = '/spabeauty';
-  static String beautycosmetics = '/beautycosmetics';
-  static String sportsclub = '/sportsclub';
-  static String artcrafthandmade = '/artcrafthandmade';
-  static String organicfoods = '/organicfoods';
+  static String glasses = "/glasses";
+  static String kidscare = "/kidscare";
+  static String fashion = "/fashion";
+  static String trinkets = "/trinkets";
+  static String bakestore = "/bakestore";
+  static String minimarket = "/minimarket";
+  static String mensonly = "/mensonly";
+  static String shoes = "/shoes";
+  static String game = "/game";
+  static String landholdings = "/landholdings";
+  static String giftsforher = "/giftsforher";
+  static String sheltering = "/sheltering";
+  static String carryall = "/carryall";
+  static String twowheeler = "/twowheeler";
+  static String housewares = "/housewares";
 }
